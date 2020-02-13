131d6f4b332d04f8702fa8ac2017f8146f6d8029
A.R. Bagas Danang Haditio <arbagasdanangh@gmail.com>
2020-02-13 10:36:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>e-Data - Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link rel="shortcut icon" href="img/logo.png" >
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <!-- Outer Row -->
+
+	
+	
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5"><img src="img/edata.jpg" style="width:100%;height:auto;" width="1280"> 
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+			<marquee class="forumtxt" width="100%" height="26" onmouseout="this.start()" onmouseover="this.stop()" scrolldelay="25" scrollamount="1" direction="left" style="background:#FFCC33">
+								<p style="background:#FF9900;color:white">  ...... S e l a m a t D a t a n g d i P o r t a l W e b D P M - P T S P P e m k o t S u r a b a y a  .....  </p>
+							</marquee>
+              <div class="col-lg-6 d-none d-lg-block">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/slide5.jpg"  alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slide4.jpg" alt="Second slide">
+	  <div class="carousel-caption">
+        <!--<h3>Slide 3 (Judul)</h3>
+		<p>Ini adalah Slide 3 (Penjelasan)</p>-->
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slide2.jpg" style="width:100%;height:100%;" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h4 class="h4 text-gray-900 mb-4">Welcome Back!</h4>
+                  </div>
+                  <form class="user" action="" method="POST">
+                    <div class="form-group">
+                      <input type="text" class="form-control form-control-user" name="username"  placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" name="password"  placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <!--<a href="index.html" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </a>-->
+					<input type="submit" class="btn btn-primary btn-user btn-block" name="login" value="Login" />
+                    <hr>
+                    <!--<a href="index.html" class="btn btn-google btn-user btn-block">
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </a>
+                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </a>-->
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.html">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
